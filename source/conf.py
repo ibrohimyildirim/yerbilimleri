c84d624fb0b6c30524e542912b959033344177e5
ibrohimyildirim <76915239+ibrohimyildirim@users.noreply.github.com>
2025-09-03 15:22:16 +0300

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -26,7 +26,6 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
 html_theme_options = [
     'collapse_navigation': True,    # Alt menüler kapalı başlar
     'sticky_navigation': True,
