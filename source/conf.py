cd8064c7031b2a51b1870cff61cdcbb305bb2e8f
ibrohimyildirim <76915239+ibrohimyildirim@users.noreply.github.com>
2025-09-03 15:33:34 +0300

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -26,6 +26,7 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
-        'collapse_navigation': True,     
+        'collapse_navigation': True,
+        'sticky_navigation': True
     }
 html_static_path = ['_static']
